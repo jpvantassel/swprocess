0d07c19ab9deab77657b5b05fa12c59ce243ca92
Joseph vantassel <jvantassel@utexas.edu>
2019-09-12 00:49:18 -0500

    :sparkles: Add zero_pad, from_trace, and from_trace_seg2, will tests

--- utprocess/test/test_timeseries.py
+++ utprocess/test/test_timeseries.py
@@ -3,8 +3,28 @@ import unittest
 import utprocess
 import obspy
 import numpy as np
+import warnings
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 
 class TestTimeSeries(unittest.TestCase):
+
+    def test_check(self):
+        for value in [True, "values", 1, 1.57]:
+            self.assertRaises(TypeError,
+                              utprocess.TimeSeries.check_input,
+                              name="blah",
+                              values=value)
+        for value in [[1, 2, 3], (1, 2, 3)]:
+            value = utprocess.TimeSeries.check_input(name="blah", values=value)
+            self.assertTrue(isinstance(value, np.ndarray))
+
+        for value in [[[1, 2], [3, 4]], ((1, 2), (3, 4)), np.array([[1, 2], [3, 4]])]:
+            self.assertRaises(TypeError,
+                              utprocess.TimeSeries.check_input,
+                              name="blah",
+                              values=value)
 
     def test_init(self):
         dt = 1
@@ -12,38 +32,62 @@ class TestTimeSeries(unittest.TestCase):
         test = utprocess.TimeSeries(amp, dt)
         self.assertListEqual(amp, test.amp.tolist())
         self.assertEqual(dt, test.dt)
-        
+
         amp = np.array(amp)
         test = utprocess.TimeSeries(amp, dt)
         self.assertListEqual(amp.tolist(), test.amp.tolist())
 
-    def test_from_seg2(self):
-        pass
+    def test_from_trace_seg2(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            trace = obspy.read("test/data/vuws/1.dat")[0]
+        tseries = utprocess.TimeSeries.from_trace_seg2(trace)
+        self.assertListEqual(tseries.amp.tolist(), trace.data.tolist())
+        self.assertEqual(tseries.dt, trace.stats.delta)
+        self.assertEqual(tseries._nstack, int(trace.stats.seg2.STACK))
+        self.assertEqual(tseries.delay, float(trace.stats.seg2.DELAY))
 
-    def test_from_miniseed(self):
-        pass
+    def test_from_trace(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            trace = obspy.read("test/data/vuws/1.dat")[0]
+        tseries = utprocess.TimeSeries.from_trace(trace, delay=-0.5)
+        self.assertListEqual(tseries.amp.tolist(), trace.data.tolist())
+        self.assertEqual(tseries.dt, trace.stats.delta)
+        self.assertEqual(tseries._nstack, int(trace.stats.seg2.STACK))
+        self.assertEqual(tseries.delay, float(trace.stats.seg2.DELAY))
 
-    def test_stack_append_1trace(self):
-        amp = [0,1,0,-1]
-        dt = 1
-        test = utprocess.TimeSeries(amp, dt)
-        new_amp = [0,-1,0,1]
-        dt = 1
-        test.stack_append(new_amp, dt)
-        self.assertListEqual(test.amp.tolist(), [0,0,0,0])
+        # TODO: add miniseed
 
-    def test_stack_5trace(self):
-        amp = [10]
-        dt = 1
-        test = utprocess.TimeSeries(amp, dt)
-        new_amp = [5]
-        dt = 1
-        nstacks = 5
-        test.stack_append(new_amp, dt, nstacks=nstacks)
+    def test_stack_append(self):
+        # Append trace with 1 stack.
+        test = utprocess.TimeSeries(amplitude=[0, 1, 0, -1], dt=1)
+        test.stack_append(amplitude=[0, -1, 0, 1], dt=1)
+        self.assertListEqual(test.amp.tolist(), [0, 0, 0, 0])
+
+        # Append trace with 5 stacks.
+        test = utprocess.TimeSeries(amplitude=[10], dt=1)
+        test.stack_append(amplitude=[5], dt=1, nstacks=5)
         true = (10*1 + 5*5)/(5+1)
         self.assertEqual(true, test.amp)
 
-    # def test_
+    def test_zero_pad(self):
+        thist = utprocess.TimeSeries(amplitude=list(np.arange(0, 2, 0.01)),
+                                     dt=0.01)
+        self.assertEqual(len(thist.amp), 200)
+        thist.zero_pad(df=0.1)
+        self.assertEqual(len(thist.amp), 1000)
+        thist.zero_pad(df=0.5)
+        self.assertEqual(len(thist.amp)/thist.multiple, 1/(0.01*0.5))
+
+        thist = utprocess.TimeSeries(amplitude=list(np.arange(0, 2, 0.02)),
+                                     dt=0.02)
+        self.assertEqual(len(thist.amp), 100)
+        thist.zero_pad(df=1)
+        self.assertEqual(len(thist.amp), 200)
+        self.assertEqual(thist.multiple, 4)
+        self.assertEqual(len(thist.amp)/thist.multiple, 1/(0.02*1))
+
 
 if __name__ == '__main__':
     unittest.main()
--- utprocess/timeseries.py
+++ utprocess/timeseries.py
@@ -3,7 +3,8 @@ time series objects.
 """
 
 import numpy as np
-import obspy
+import logging
+logger = logging.getLogger(__name__)
 
 
 class TimeSeries():
@@ -15,7 +16,12 @@ class TimeSeries():
         dt: Float denoting the time step between samples in seconds.
     """
     @staticmethod
-    def __check_input(name, values):
+    def check_input(name, values):
+        """Check 'values' is np.ndarray, list, or tuple. 
+        If it is a list or tuple convert it to a np.ndarray. 
+        Ensure 'values' is one-dimensional np.ndarray.
+        'name' is only used to raise easily understood exceptions.
+        """
         if type(values) not in [np.ndarray, list, tuple]:
             raise TypeError("{} must be of type np.array, list, or tuple not {}".format(
                 name, type(values)))
@@ -26,6 +32,7 @@ class TimeSeries():
         if len(values.shape) > 1:
             raise TypeError("{} must be 1-dimensional, not {}-dimensional".format(
                 name, values.shape))
+        return values
 
     def __init__(self, amplitude, dt, nstacks=1, delay=0):
         """Initialize a TimeSeries object.
@@ -50,28 +57,132 @@ class TimeSeries():
         Exceptions:
             This method raises no exceptions.
         """
-        TimeSeries.__check_input("amplitude", amplitude)
+        amplitude = TimeSeries.check_input("amplitude", amplitude)
 
         if type(amplitude) == np.ndarray:
             self.amp = amplitude
         else:
             self.amp = np.array(amplitude)
+        self.nsamples = len(self.amp)
         self.dt = dt
         self.fs = 1/self.dt
         self.fnyq = 0.5*self.fs
+        self.df = self.fs/self.nsamples
         self._nstack = 1
+        assert(delay <= 0)
         self.delay = delay
-        self.nsamples = len(self.amp)
+        self.multiple = 1
+
+        logging.info(f"Initialize a TimeSeries object.")
+        logging.info(f"\tdt = {dt}")
+        logging.info(f"\tfs = {self.fs}")
+        logging.info(f"\tdelay = {delay}")
+        logging.info(f"\tnsamples = {self.nsamples}")
+
+    def zero_pad(self, df=0.2):
+        """Append zeros to the end of the TimeSeries object to achieve a
+        desired frequency step.
+
+        Args:
+            df: Positive float that denotes the desired frequency step
+                in Hertz.
+
+        Returns:
+            Returns no value, instead modifies the TimeSeries object
+            attributes:
+                amp
+                nsamples
+                multiple
+
+        Raises:
+            Raises a value error if df is not a positive number.
+        """
+        if isinstance(df, (float, int)):
+            if df <= 0:
+                raise ValueError(f"df must be positive.")
+        else:
+            raise TypeError(f"df must be `float` or `int`, not {type(df)}.")
+
+        nreq = int(np.round(1/(df*self.dt)))
+
+        logging.info(f"nreq = {nreq}")
+
+        # If nreq > nsamples, padd zeros to achieve nsamples = nreq
+        if nreq > self.nsamples:
+            self.amp = np.concatenate((self.amp,
+                                      np.zeros(nreq - self.nsamples)))
+            self.nsamples = nreq
+
+        # If nreq <= nsamples, padd zeros to achieve a fraction of df (e.g., df/2)
+        # After processing, extract the results at the frequencies of interest
+        else:
+            # Multiples of df and nreq
+            multiples = np.array([1, 2, 4, 8, 16, 32], int)
+            trial_n = nreq*multiples
+
+            logging.debug(f"trial_n = {trial_n}")
+
+            # Find first trial_n > nreq
+            self.multiple = multiples[np.where(self.nsamples < trial_n)[0][0]]
+            logging.debug(f"multiple = {self.multiple}")
+
+            self.amp = np.concatenate((self.amp,
+                                      np.zeros(nreq*self.multiple - self.nsamples)))
+            self.nsamples = nreq*self.multiple
+            
+            logging.debug(f"nsamples = {self.nsamples}")
 
     @classmethod
-    def from_trace(cls, trace):
+    def from_trace_seg2(cls, trace):
+        """Initialize a TimeSeries object from a seg2 trace object.
+
+        This method is similar to from_trace except that it extracts
+        additional information from the trace header. So only use this 
+        method if you have a seg2 file and the header information is 
+        correct.
+
+        Args:
+            trace: Trace object from a correctly written seg2 file.
+
+        Returns:
+            Initialized TimeSeries object.
+
+        Raises:
+            This method raises no exceptions.
+        """
         return cls(amplitude=trace.data,
                    dt=trace.stats.delta,
-                   nstacks=int(trace.stats.seg2.STACK))
+                   nstacks=int(trace.stats.seg2.STACK),
+                   delay=float(trace.stats.seg2.DELAY))
 
     @classmethod
-    def from_miniseed(cls, filename):
-        pass
+    def from_trace(cls, trace, nstacks=1, delay=0):
+        """Initialize a TimeSeries object from a trace object.
+
+        This method is a more general method than from_trace_seg2, which
+        does not attempt to extract anything from the trace object 
+        except for its data and sampling step.
+
+        Args:
+            trace: Trace object.
+
+            nstacks: Integer representing the number of stacks. Default 
+                value is one (i.e., only a single recording was made).
+
+            delay: Number that is less than or equal to zero, denoting
+                the pre-trigger delay. Default value is zero (i.e., no
+                pre-trigger delay was recorded).
+
+        Returns:
+            Initialized TimeSeries object.
+
+        Raises:
+            This method raises no exceptions.
+        """
+        return cls(amplitude=trace.data,
+                   dt=trace.stats.delta,
+                   nstacks=nstacks,
+                   delay=delay)
 
     def stack_append(self, amplitude, dt, nstacks=1):
         """Stack (i.e., average) a new time series to the current time
@@ -94,10 +205,10 @@ class TimeSeries():
         Raises:
             TypeError: If amplitude is not an np.array or list.
 
-            IndexError: If length of the amplitude does not match the 
+            IndexError: If the length of amplitude does not match the
                 length of the current time series.
         """
-        TimeSeries.__check_input("amplitude", amplitude)
+        amplitude = TimeSeries.check_input("amplitude", amplitude)
 
         if len(amplitude) != len(self.amp):
             raise IndexError("Length of two waveforms must be the same.")
@@ -105,11 +216,9 @@ class TimeSeries():
         if type(amplitude) is list:
             amplitude = np.array(amplitude)
 
-        self.amp = (self.amp*self._nstack + amplitude *
-                    nstacks)/(self._nstack+nstacks)
-        # print(self.amp)
+        self.amp = (self.amp*self._nstack + amplitude*nstacks) / \
+            (self._nstack+nstacks)
         self._nstack += nstacks
-        # print(self._nstack)
 
     def __repr__(self):
         # """Valid python expression to reproduce the object"""
