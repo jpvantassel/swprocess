f32bb009449cc73db0b34979c873a68e9606db82
Joseph vantassel <jvantassel@utexas.edu>
2020-01-16 16:17:26 -0600

    :wrench: Update setup file to latest

--- setup.py
+++ setup.py
@@ -1,21 +1,54 @@
-import setuptools
+"""A setuptools based setup module.
 
-with open("README.md", "r") as f:
+See:
+https://packaging.python.org/guides/distributing-packages-using-setuptools/
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
     long_description = f.read()
 
-setuptools.setup(
-    name="utprocess",
-    version="0.0.1",
-    author="Joseh P Vantassel",
-    author_email="jvantassel@utexas.edu",
-    description="Tools for surface wave processing.",
+setup(
+    name='utprocess',
+    version='0.1.0',
+    description='Package for Surface-Wave Processing',
     long_description=long_description,
-    long_description_content_type="text/markdown.-",
-    url="https://github.com/jpvantassel/utinvert",
-    packages=setuptools.find_packages(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/jpvantassel/utprocess',
+    author='Joseph P. Vantassel',
+    author_email='jvantassel@utexas.edu',
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "Liscence :: OSI Approvied :: MIT Liscence",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
+    keywords='surface-wave processing geopsy active passive',
+    packages=find_packages(),
+    python_requires = '>3.6',
+    install_requires=["numpy", "scipy", "matplotlib", "obspy"],
+    extras_require={
+        'dev': ['unittest', 'hypothesis'],
+    },
+    package_data={
+    },
+    data_files=[
+    ],
+    entry_points={  
+    },
+    project_urls={
+    },
 )
