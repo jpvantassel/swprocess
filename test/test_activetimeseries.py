8634c2aef7cc680a001e63b417b86af18f15ff74
Joseph vantassel <jvantassel@utexas.edu>
2020-03-10 19:14:47 -0500

    :sparkles: Change TimeSeries to subclass ActiveTimeSeries

---
+++ test/test_activetimeseries.py
@@ -0,0 +1,242 @@
+"""Tests for ActiveTimeSeries class. """
+
+import matplotlib.pyplot as plt
+from testtools import unittest, TestCase
+import utprocess
+import obspy
+import numpy as np
+import warnings
+import logging
+logging.basicConfig(level=logging.WARNING)
+
+
+class Test_ActiveTimeSeries(TestCase):
+
+    def test_check(self):
+        for value in ["values", ["a", "b", "c"]]:
+            self.assertRaises(TypeError,
+                              utprocess.ActiveTimeSeries._check_input,
+                              name="blah",
+                              values=value)
+        for value in [[1, 2, 3], (1, 2, 3), [[1, 2, 3], [4, 5, 6]]]:
+            value = utprocess.ActiveTimeSeries._check_input(
+                name="blah", values=value)
+            self.assertTrue(isinstance(value, np.ndarray))
+
+    def test_init(self):
+        dt = 1
+        amp = [0, 1, 0, -1]
+        returned = utprocess.ActiveTimeSeries(amp, dt)
+        self.assertArrayEqual(np.array(amp), returned.amp)
+        self.assertEqual(dt, returned.dt)
+
+    def test_time(self):
+        dt = 0.5
+        amp = [0, 1, 2, 3]
+        obj = utprocess.ActiveTimeSeries(amp, dt)
+
+        expected = np.array([0., 0.5, 1., 1.5])
+        returned = obj.time
+        self.assertArrayEqual(expected, returned)
+
+        # With pre-event delay
+        dt = 0.5
+        amp = [-1, 0, 1, 0, -1]
+        true_time = [-0.5, 0., 0.5, 1., 1.5]
+        test = utprocess.ActiveTimeSeries(amp, dt, delay=-0.5)
+        self.assertListEqual(test.time.tolist(), true_time)
+
+    def test_from_trace_seg2(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            trace = obspy.read("test/data/vuws/1.dat")[0]
+        returned = utprocess.ActiveTimeSeries.from_trace_seg2(trace)
+        self.assertArrayEqual(trace.data, returned.amp)
+        self.assertEqual(trace.stats.delta, returned.dt)
+        self.assertEqual(int(trace.stats.seg2.STACK), returned._nstack)
+        self.assertEqual(float(trace.stats.seg2.DELAY), returned.delay)
+
+    def test_from_trace(self):
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            trace = obspy.read("test/data/vuws/1.dat")[0]
+        tseries = utprocess.ActiveTimeSeries.from_trace(trace, delay=-0.5)
+        self.assertListEqual(tseries.amp.tolist(), trace.data.tolist())
+        self.assertEqual(tseries.dt, trace.stats.delta)
+        self.assertEqual(tseries._nstack, int(trace.stats.seg2.STACK))
+        self.assertEqual(tseries.delay, float(trace.stats.seg2.DELAY))
+
+    def test_stack_append(self):
+        # Append trace with 1 stack.
+        tseries = utprocess.ActiveTimeSeries(amplitude=[0, 1, 0, -1], dt=1)
+        tseries.stack_append(amplitude=[0, -1, 0, 1], dt=1)
+        returned = tseries.amp
+        expected = np.array([0, 0, 0, 0])
+        self.assertArrayEqual(expected, returned)
+
+        # Append trace with 5 stacks.
+        tseries = utprocess.ActiveTimeSeries(amplitude=[10], dt=1)
+        tseries.stack_append(amplitude=[5], dt=1, n_stacks=5)
+        returned = tseries.amp
+        expected = (10*1 + 5*5)/(5+1)
+        self.assertEqual(expected, returned)
+
+    # def test_zero_pad(self):
+    #     thist = utprocess.ActiveTimeSeries(amplitude=list(np.arange(0, 2, 0.01)),
+    #                                        dt=0.01)
+    #     self.assertEqual(len(thist.amp), 200)
+    #     thist.zero_pad(df=0.1)
+    #     self.assertEqual(len(thist.amp), 1000)
+    #     thist.zero_pad(df=0.5)
+    #     self.assertEqual(len(thist.amp)/thist.multiple, 1/(0.01*0.5))
+
+    #     thist = utprocess.ActiveTimeSeries(amplitude=list(np.arange(0, 2, 0.02)),
+    #                                        dt=0.02)
+    #     self.assertEqual(len(thist.amp), 100)
+    #     thist.zero_pad(df=1)
+    #     self.assertEqual(len(thist.amp), 200)
+    #     self.assertEqual(thist.multiple, 4)
+    #     self.assertEqual(len(thist.amp)/thist.multiple, 1/(0.02*1))
+
+    def test_trim(self):
+        # Standard
+        thist = utprocess.ActiveTimeSeries(amplitude=np.arange(0, 2, 0.001),
+                                           dt=0.001)
+        self.assertEqual(len(thist.amp), 2000)
+        thist.trim(0, 1)
+        self.assertEqual(len(thist.amp), 1000)
+        self.assertEqual(thist.n_samples, 1000)
+
+        # With pre-trigger delay
+        thist = utprocess.ActiveTimeSeries(amplitude=np.arange(0, 2, 0.001),
+                                           dt=0.001,
+                                           delay=-.5)
+        # Remove part of pre-trigger
+        thist.trim(-0.25, 0.25)
+        self.assertEqual(thist.n_samples, 500)
+        self.assertEqual(thist.delay, -0.25)
+        # Remove all of pre-trigger
+        thist.trim(0, 0.2)
+        self.assertEqual(thist.n_samples, 200)
+        self.assertEqual(thist.delay, 0)
+
+        # With pre-trigger delay
+        dt = 0.001
+        thist = utprocess.ActiveTimeSeries(amplitude=np.arange(0, 2, dt),
+                                          dt=dt,
+                                          delay=-.5)
+        # Remove part of pre-trigger
+        thist.trim(-0.25, 0.25)
+        self.assertEqual(thist.n_samples, 500)
+        self.assertEqual(thist.delay, -0.25)
+        self.assertAlmostEqual(min(thist.time), -0.25, delta=dt)
+        self.assertAlmostEqual(max(thist.time), +0.25-dt, delta=dt)
+        # Remove all of pre-trigger
+        thist.trim(0, 0.25)
+        self.assertEqual(thist.n_samples, 250)
+        self.assertAlmostEqual(thist.delay, 0.001, delta=dt)
+        self.assertAlmostEqual(min(thist.time), 0, delta=dt)
+        self.assertAlmostEqual(max(thist.time), 0.25-dt, delta=dt)
+
+    def test_crosscorr(self):
+        ampa = [0, 1, 0]
+        ampb = [1]
+        dt = 1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        correlate = utprocess.ActiveTimeSeries.crosscorr(tseries_a, tseries_b)
+        self.assertListEqual(correlate.tolist(), [0, 1, 0])
+
+        ampa = [0, 0, 1, 0]
+        ampb = [1]
+        dt = 1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        correlate = utprocess.ActiveTimeSeries.crosscorr(tseries_a, tseries_b)
+        self.assertListEqual(correlate.tolist(), [0, 0, 1, 0])
+
+        ampa = [0, 0, 1, 0]
+        ampb = [1, 0]
+        dt = 1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        correlate = utprocess.ActiveTimeSeries.crosscorr(tseries_a, tseries_b)
+        self.assertListEqual(correlate.tolist(), [0, 0, 0, 1, 0])
+
+        ampa = [0, 0, 1, 0]
+        ampb = [0, 1, 0]
+        dt = 1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        correlate = utprocess.ActiveTimeSeries.crosscorr(tseries_a, tseries_b)
+        self.assertListEqual(correlate.tolist(), [0, 0, 0, 1, 0, 0])
+
+        ampa = [0, 0, -1, 0]
+        ampb = [0, -1, 0, 0]
+        dt = 1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        correlate = utprocess.ActiveTimeSeries.crosscorr(tseries_a, tseries_b)
+        self.assertListEqual(correlate.tolist(), [0, 0, 0, 0, 1, 0, 0])
+
+    def test_crosscorr_shift(self):
+        # Simple Pulse
+        ampa = [0, 0, 1, 0]
+        ampb = [0, 1, 0, 0]
+        dt = 1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        shifted = utprocess.ActiveTimeSeries.crosscorr_shift(tseries_a, tseries_b)
+        self.assertListEqual(shifted.tolist(), ampa)
+
+        # Simple Pulse
+        ampa = [0, 0, 2, 3, 4, 5, 0, 2, 3, 0]
+        ampb = [0, 2, 3, 4, 5, 0, 0, 0, 0, 0]
+        dt = 1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        shifted = utprocess.ActiveTimeSeries.crosscorr_shift(tseries_a, tseries_b)
+        self.assertListEqual(shifted.tolist(), [0, 0, 2, 3, 4, 5, 0, 0, 0, 0])
+
+        # Sinusoidal Pulse
+        ampa = [0, -1, 0, 1, 0, -1, 0, 0]
+        ampb = [0, 0, 0, -1, 0, 1, 0, 0]
+        dt = 0.1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        shifted = utprocess.ActiveTimeSeries.crosscorr_shift(tseries_a, tseries_b)
+        self.assertListEqual(shifted.tolist(), [0, -1, 0, 1, 0, 0, 0, 0])
+
+        # Sinusoid
+        dt = 0.01
+        time = np.arange(0,2,dt)
+        f = 5
+        ampa = np.concatenate((np.zeros(20), np.sin(2*np.pi*f*time)))
+        ampb = np.concatenate((np.sin(2*np.pi*f*time),np.zeros(20)))
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        shifted = utprocess.ActiveTimeSeries.crosscorr_shift(tseries_a, tseries_b)
+        self.assertListEqual(shifted.tolist(), ampa.tolist())
+
+    def test_cross_stack(self):
+        # Simple pulse
+        ampa = [0, 0, 1, 0]
+        ampb = [0, 1, 0, 0]
+        dt = 1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        stacked = utprocess.ActiveTimeSeries.from_cross_stack(tseries_a, tseries_b)
+        self.assertListEqual(stacked.amp.tolist(), ampa)
+
+        # Simple Sinusoid
+        ampa = [0, 0, 0, 1, 0, 1, 0, 0, 0, 0]
+        ampb = [0, 0, 0, 0, 0, 0, 1, 0, 1, 0]
+        dt = 1
+        tseries_a = utprocess.ActiveTimeSeries(ampa, dt)
+        tseries_b = utprocess.ActiveTimeSeries(ampb, dt)
+        stacked = utprocess.ActiveTimeSeries.from_cross_stack(tseries_a, tseries_b)
+        self.assertListEqual(stacked.amp.tolist(), ampa)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ utprocess/activetimeseries.py
@@ -0,0 +1,328 @@
+"""This file contains the class ActiveTimeSeries."""
+
+import numpy as np
+import scipy.signal as signal
+from sigpropy import TimeSeries
+import warnings
+import logging
+logger = logging.getLogger(__name__)
+
+
+class ActiveTimeSeries(TimeSeries):
+    """A class for working with active-source TimeSeries.
+
+    Attributes
+    ----------
+    amp : ndarray
+        Recording's amplitude, one per sample.
+    dt : float
+        Time step between samples in seconds.
+    
+    """
+    # @staticmethod
+    # def check_input(name, values):
+    #     """Check 'values' is `ndarray`, `list`, or `tuple`. 
+    #     If it is a list or tuple convert it to a np.ndarray. 
+    #     Ensure 'values' is one-dimensional np.ndarray.
+    #     'name' is only used to raise easily understood exceptions.
+    #     """
+    #     if type(values) not in [np.ndarray, list, tuple]:
+    #         raise TypeError("{} must be of type np.array, list, or tuple not {}".format(
+    #             name, type(values)))
+
+    #     if isinstance(values, (list, tuple)):
+    #         values = np.array(values)
+
+    #     if len(values.shape) > 1:
+    #         raise TypeError("{} must be 1-dimensional, not {}-dimensional".format(
+    #             name, values.shape))
+    #     return values
+
+    def __init__(self, amplitude, dt, n_stacks=1, delay=0):
+        """Initialize a `TimeSeries` object.
+
+        Parameters
+        ----------
+        amplitude : array-like
+            Recording's amplitude, one per sample. The first 
+            value is associated with time=0 seconds and the last is 
+            associate with (len(amplitude)-1)*dt seconds.
+        dt : float
+            Time step between samples in seconds.
+        n_stacks : int, optional
+            Number of stacks used to produce `amplitude`, default is 1.
+        delay : float
+            Delay to the start of the record in seconds.
+
+        Returns
+        -------
+        TimeSeries
+            Intialized `TimeSeries` object.
+        """
+        super().__init__(amplitude=amplitude, dt=dt)
+  
+        self._nstack = 1
+        assert(delay <= 0)
+        self.delay = delay
+
+    @property
+    def time(self):
+        """Return time vector for `ActiveTimeSeries` object."""
+        if self.n_windows == 1:
+            return np.arange(0, self.n_samples*self.dt, self.dt) + self.delay
+        else:
+            samples_per_window = (self.n_samples//self.n_windows)+1
+            time = np.zeros((self.n_windows, samples_per_window))
+            for cwindow in range(self.n_windows):
+                start_time = cwindow*(samples_per_window-1)*self.dt
+                stop_time = start_time + (samples_per_window-1)*self.dt
+                time[cwindow] = np.linspace(
+                    start_time, stop_time, samples_per_window)
+            return time
+
+
+    def trim(self, start_time, end_time):
+        """Trim excess from time series in the half-open interval
+        [start_time, end_time).
+
+        Parameters
+        ----------
+        start_time : float
+            New time zero in seconds.
+        end_time : float
+            New end time in seconds. Note that the interval is
+            half-open.
+
+        Returns
+        -------
+        None
+            Updates the attributes `n_samples`, `delay`, and `df`.
+
+        Raises
+        ------
+        IndexError
+            If the `start_time` and `end_time` is illogical.
+            For example, `start_time` is before the start of the
+            `delay` or after `end_time`, or the `end_time` is
+            after the end of the record.
+        """
+        current_time = self.time
+        start = min(current_time)
+        end = max(current_time)
+        
+        if start_time > end_time:
+            logger.debug(f"{start} <= {start_time} < {end_time}: Must be True.")
+            raise IndexError("Illogical start_time, see doctring.")
+
+        if end_time < start_time:
+            logger.debug(f"{start_time} < {end_time} < {end}: Must be True.")
+            raise IndexError("Illogical end_time, see doctring.")
+
+        logger.info(f"start = {start}, moving to start_time = {start_time}")
+        logger.info(f"start = {end}, moving to end_time = {end_time}")
+
+        start_index = np.argmin(np.absolute(current_time - start_time))
+        end_index = np.argmin(np.absolute(current_time - end_time))
+
+        if end_time > (end - self.dt/2):
+            msg = "end_time beyond end of record, setting to true end_time."
+            warnings.warn(msg)
+            end_index = len(current_time)
+        if start_time < (start + self.dt/2):
+            msg = "start_time before start of record, setting to start_time."
+            warnings.warn(msg)
+            start_index = 0
+
+        logger.debug(f"start_index = {start_index}")
+        logger.debug(f"start_index = {end_index}")
+
+        self.amp = self.amp[start_index:end_index]
+        self.n_samples = len(self.amp)
+        self._df = self.fs/self.n_samples
+        self.delay = 0 if start_time >= 0 else start_time
+
+        logger.info(f"n_samples = {self.n_samples}")
+        logger.info(f"df = {self._df}")
+        logger.info(f"delay = {self.delay}")
+
+    def zero_pad(self, df=0.2):
+        """Append zeros to `amp` to achieve a desired frequency step.
+
+        Parameters
+        ----------
+        df : float
+            Desired frequency step in Hertz.
+
+        Returns
+        -------
+        None
+            Instead modifies attributes: `amp`, `n_samples`, `multiple`.
+
+        Raises
+        ------
+        ValueError
+            If `df` < 0 (i.e., non-positive).
+        """
+        raise NotImplementedError
+        if isinstance(df, (float, int)):
+            if df <= 0:
+                raise ValueError(f"df must be positive.")
+        else:
+            raise TypeError(f"df must be `float` or `int`, not {type(df)}.")
+
+        nreq = int(np.round(1/(df*self.dt)))
+
+        logging.info(f"nreq = {nreq}")
+
+        # If nreq > n_samples, padd zeros to achieve n_samples = nreq
+        if nreq > self.n_samples:
+            self.amp = np.concatenate((self.amp,
+                                       np.zeros(nreq - self.n_samples)))
+            self.n_samples = nreq
+
+        # If nreq <= n_samples, padd zeros to achieve a fraction of df (e.g., df/2)
+        # After processing, extract the results at the frequencies of interest
+        else:
+            # Multiples of df and nreq
+            multiples = np.array([1, 2, 4, 8, 16, 32], int)
+            trial_n = nreq*multiples
+
+            logging.debug(f"trial_n = {trial_n}")
+
+            # Find first trial_n > nreq
+            self.multiple = multiples[np.where(self.n_samples < trial_n)[0][0]]
+            logging.debug(f"multiple = {self.multiple}")
+
+            self.amp = np.concatenate((self.amp,
+                                       np.zeros(nreq*self.multiple - self.n_samples)))
+            self.n_samples = nreq*self.multiple
+
+            logging.debug(f"n_samples = {self.n_samples}")
+
+    @classmethod
+    def from_trace_seg2(cls, trace):
+        """Initialize a `TimeSeries` object from a SEG2 `Trace` object.
+
+        This method is similar to meth:`from_trace` except that it
+        extracts additional information from the `Trace` header. So only
+        use this method if you have a SEG2 file and the header
+        information is correct.
+
+        Parameters
+        ----------
+        trace: Trace 
+            `Trace` object from a correctly written seg2 file.
+
+        Returns
+        -------
+        ActiveTimeSeries
+            Instantiated with seg2 file information.
+        """
+        return cls.from_trace(trace = trace,
+                              n_stacks=int(trace.stats.seg2.STACK),
+                              delay=float(trace.stats.seg2.DELAY))
+
+    def stack_append(self, amplitude, dt, n_stacks=1):
+        """Stack (i.e., average) a new time series into the current oen.
+
+        Parameters
+        ----------
+        amplitude : array-like
+            New amplitiude to be stacked (i.e., averaged with the
+            current time series).
+        dt : float
+            Time step of the new time series in seconds. Only used for
+            comparison with the current time series.
+        n_stacks : int
+            Number of stacks used to produce `amplitude`, default is 1.
+
+        Returns
+        -------
+        None
+            Instead updates the attribute `amp`.
+
+        Raises:
+            TypeError: If amplitude is not an np.array or list.
+
+            IndexError: If the length of amplitude does not match the
+                length of the current time series.
+        """
+        amplitude = ActiveTimeSeries._check_input("amplitude", amplitude)
+
+        if len(amplitude) != len(self.amp):
+            raise IndexError("Length of two waveforms must be the same.")
+
+        if type(amplitude) is list:
+            amplitude = np.array(amplitude)
+
+        self.amp = (self.amp*self._nstack + amplitude*n_stacks) / \
+            (self._nstack+n_stacks)
+        self._nstack += n_stacks
+
+    @classmethod
+    def from_trace(cls, trace, n_stacks=1, delay=0):
+        """Initialize a `TimeSeries` object from a trace object.
+
+        This method is more general method than `from_trace_seg2`, 
+        as it does not attempt to extract any metadata from the `Trace` 
+        object.
+
+        Parameters
+        ----------
+        trace : Trace
+            Refer to
+            `obspy documentation <https://github.com/obspy/obspy/wiki>`_
+            for more information
+
+        n_stacks : int, optional
+            Number of stacks the time series represents, (default is
+            1, signifying a single unstacked time record).
+
+        delay : float {<=0.}, optional
+            Denotes the pre-event delay, (default is zero, 
+            meaning no pre-event noise was recorded).
+
+        Returns
+        -------
+        TimeSeries
+            Initialized with information from `trace`.
+        """
+        return cls(amplitude=trace.data, dt=trace.stats.delta,
+                   n_stacks=n_stacks, delay=delay)
+
+    @staticmethod
+    def crosscorr(timeseries_a, timeseries_b):
+        """Return cross correlation of two timeseries objects."""
+        # TODO (jpv): Create method for comparing tseriesa == tseriesb
+        return signal.correlate(timeseries_a.amp, timeseries_b.amp)
+
+    @staticmethod
+    def crosscorr_shift(timeseries_a, timeseries_b):
+        """Return shifted timeseries_b so that it is maximally
+        corrlated with tiemseries_a."""
+        corr = ActiveTimeSeries.crosscorr(timeseries_a, timeseries_b)
+        # print(corr)
+        # print(np.where(abs(corr) == max(abs(corr))))
+        maxcorr_location = np.where(corr == max(corr))[0][0]
+        # print(maxcorr_location)
+        shifts = (maxcorr_location+1)-timeseries_b.n_samples
+        # print(shifts)
+        # print(type(shifts))
+        if shifts > 0:
+            return np.concatenate((np.zeros(shifts), timeseries_b.amp[:-shifts]))
+        elif shifts < 0:
+            return np.concatenate((timeseries_b.amp[abs(shifts):], np.zeros(abs(shifts))))
+        else:
+            return timeseries_b.amp
+
+    @classmethod
+    def from_cross_stack(cls, timeseries_a, timeseries_b):
+        """Return a single trace that is the result of stacking two 
+        time series, which are aligned using cross-correlation."""
+        obj = cls(timeseries_a.amp, timeseries_a.dt)
+        shifted_amp = cls.crosscorr_shift(timeseries_a, timeseries_b)
+        obj.stack_append(shifted_amp, timeseries_b.dt)
+        return obj
+
+    def __repr__(self):
+        return f"ActiveTimeSeries(dt={self.dt}, amplitude={str(self.amp[0:3])[:-1]} ... {str(self.amp[-3:])[1:]})"
