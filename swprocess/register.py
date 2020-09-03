9a4204d5fd144229db79e979087aa4b5416b8d99
Joseph vantassel <jvantassel@utexas.edu>
2020-09-02 21:14:47 -0500

    :sparkles: New register class for factory design pattern

---
+++ swprocess/register.py
@@ -0,0 +1,33 @@
+"""Registry class definition."""
+
+from abc import ABCMeta
+import logging
+
+from wavefieldtransforms import FK, SlantStack, PhaseShift, FDBF
+
+
+class AbstractRegistry(ABCMeta):
+
+    @classmethod
+    def register(cls, name):
+
+        def wrapper(class_to_wrap):
+            if name in cls._register:
+                logger.warning(f"Key {name} already exists, replacing ...")
+            cls._register[name] = class_to_wrap
+            return class_to_wrap
+
+        return wrapper
+
+    @classmethod
+    def create_instance(cls, name, *args, **kwargs):
+        instance = cls._register[name]
+        return instance(*args, **kwargs)
+
+class WavefieldTransformRegistry(AbstractRegistry):
+
+    _register = {}
+
+    def __init__(self, name):
+        super.__init__(name)
+    
